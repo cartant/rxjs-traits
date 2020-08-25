d43f9bc8d221cbd8140daa4dd600644d55c470e4
Nicholas Jamieson <nicholas@cartant.com>
2020-08-25 16:44:24 +1000

    feat: Add observe/subscribeOn.

---
+++ dtslint/rxjs/operators/observeOn.ts
@@ -0,0 +1,20 @@
+/**
+ * @license Use of this source code is governed by an MIT-style license that
+ * can be found in the LICENSE file at https://github.com/cartant/rxjs-traits
+ */
+
+import { asapScheduler } from "rxjs";
+import { of } from "../../../source/rxjs";
+import { observeOn } from "../../../source/rxjs/operators";
+import { as } from "../as";
+
+describe("observeOn", () => {
+  it("should support observeOn", () => {
+    const source = of(1, 2, 3);
+    const result = source.pipe(observeOn(asapScheduler));
+    const async = as(result, "async"); // $ExpectType true
+    const complete = as(result, "complete"); // $ExpectType true
+    const max = as(result, "max"); // $ExpectType 3
+    const min = as(result, "min"); // $ExpectType 3
+  });
+});
---
+++ source/rxjs/operators/subscribeOn.ts
@@ -0,0 +1,27 @@
+/**
+ * @license Use of this source code is governed by an MIT-style license that
+ * can be found in the LICENSE file at https://github.com/cartant/rxjs-traits
+ */
+
+import * as root from "rxjs";
+import * as operators from "rxjs/operators";
+import {
+  Observable,
+  ObservableElement,
+  ObservableTraits,
+  Operator,
+} from "../Observable";
+
+export function subscribeOn<TSource extends Observable>(
+  scheduler: root.SchedulerLike
+) {
+  return operators.subscribeOn(scheduler) as Operator<
+    TSource,
+    Observable<
+      ObservableElement<TSource>,
+      Omit<ObservableTraits<TSource>, "async"> & {
+        async: true;
+      }
+    >
+  >;
+}
